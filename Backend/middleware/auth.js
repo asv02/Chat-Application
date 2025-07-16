34d5236e8f727272fdf8f7ec84a3ec2c1bb8ff1e
asv02 <200106025@hbtu.ac.in>
2025-07-17 00:38:53 +0530

    done bearer token

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,28 +1,25 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/User')
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-const auth = async (req, res, next) => {
+module.exports = async function (req, res, next) {
+    const authHeader = req.headers['authorization'];
+    console.log('authHeader->', authHeader)
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+    const token = authHeader.split(' ')[1];
     try {
-        const token = req.cookies['access_token'];
-        if (!token) {
-            return res.status(401).json({ message: 'No token provided' });
+        // console.log('varifying user.....')
+        // console.log('decoded->');
+        // console.log("secret key->",process.env.JWT_SECRET_KEY)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        // console.log('decoded->',decoded);
+        req.user = await User.findOne({ where: { ContactNumber: decoded.contact } });
+        if (!req.user) {
+            return res.status(401).json({ message: 'Invalid token: user not found' });
         }
-        let temp;
-        try {
-            temp = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        } catch (err) {
-            return res.status(401).json({ message: 'Invalid token' });
-        }
-        const contact = temp.contact;
-        const user = await User.findOne({ where: { ContactNumber: contact } });
-        if (!user) {
-            return res.status(400).json({ message: 'No user found.' });
-        }
-        req.user = user;
         next();
     } catch (err) {
-        res.status(500).json({ message: 'Authentication failed', error: err.message });
+        return res.status(401).json({ message: `Invalid token ${err.message}` });
     }
-}
-
-module.exports = auth;
+};
